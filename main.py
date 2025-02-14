b56210a7d19ebfcd45c3781d11fece53a2dcb5c5
Andrej235 <andrej.nenadic06@gmail.com>
2025-02-14 12:44:22 +0100

    Create the feed forward function

--- main.py
+++ main.py
@@ -4,16 +4,29 @@ import numpy as np;
 
 Layer = namedtuple('Layer', ['weights', 'biases'])
 
-layerSizes = [2, 3, 2]
+layerSizes = [2, 3, 5]
 layers = np.zeros([len(layerSizes), 2])
 
 def init_layers():
-    layers = [];
+    weights = [];
+    biases = [];
     rng = np.random.default_rng(seed=1)
     for i in range(len(layerSizes) - 1):
-        layers += Layer(rng.standard_normal((layerSizes[i], layerSizes[i + 1])), rng.standard_normal(layerSizes[i + 1]))
+        weights.append(rng.standard_normal((layerSizes[i + 1], layerSizes[i])))
+        biases.append(rng.standard_normal(layerSizes[i + 1]))
 
-    return layers
+    return weights, biases
 
-layers = np.array(init_layers(), dtype=Layer)
-print(layers)
+def feed_forward(input, l):
+    if l == len(layerSizes) - 1:
+        return input
+    
+    output = sigmoid(np.add(np.dot(weights[l], input), biases[l]))
+    return feed_forward(output, l + 1)
+
+def sigmoid(x):
+    return np.divide(1, np.add(1, np.exp(-x)))
+
+weights, biases = init_layers()
+output = feed_forward(np.array([0., 1.]), 0)
+print(output)
