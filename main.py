15a247b07031f756c9ed6f654cf0af70d6d2723d
Andrej235 <andrej.nenadic06@gmail.com>
2025-02-15 18:03:45 +0100

    Create a 'train' method for neural networks

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import numpy as np
 from nn import NeuralNetwork
 
-nn = NeuralNetwork(layer_sizes=[2, 3, 5])
-input_data = np.array([0.0, 1.0])
-output = nn.forward(input_data)
-target = np.array([0, 0, 0, 1, 0])
-print(nn.calculate_cost(output, target))
-nn.backward(target)
+nn = NeuralNetwork(layer_sizes=[28*28, 50, 10])
+nn.train(10, 0.1)
--- nn.py
+++ nn.py
@@ -1,4 +1,5 @@
 import numpy as np;
+import mnist
 
 class NeuralNetwork:
     def __init__(self, layer_sizes, seed=1):
@@ -51,7 +52,39 @@ class NeuralNetwork:
             bias_gradient[l] = base
             weight_gradient[l] = np.outer(base, self.activations[l])
             
-        for i in range(len(self.weights)):
-            print(f"Weight {i + 1}:")
-            print(self.weights[i].shape)
-            print(weight_gradient[i].shape)
+        return weight_gradient, bias_gradient
+            
+    def load_mnist(self, filename):
+        with open(filename, 'rb') as fd:
+            return mnist.parse_idx(fd)
+    
+    def train(self, epochs, learning_rate):
+        inputs = self.load_mnist('./mnist/train-images.idx3-ubyte').reshape(60000, 784).astype(np.float32) / 255.0
+        expected_outputs = np.eye(10)[self.load_mnist('./mnist/train-labels.idx1-ubyte')]
+        
+        rng = np.random.default_rng(1)
+        num_samples = inputs.shape[0]
+        for epoch in range(epochs):
+            total_cost = 0
+            
+            indices = np.arange(num_samples)
+            rng.shuffle(indices)
+
+            for i in indices:
+                x = inputs[i]
+                y_expected = expected_outputs[i]
+
+                output = self.forward(x)
+                
+                cost = self.calculate_cost(output, y_expected)
+                total_cost += cost
+
+                weight_gradient, bias_gradient = self.backward(y_expected)
+
+                for l in range(len(self.weights)):
+                    self.weights[l] -= learning_rate * weight_gradient[l]
+                    self.biases[l] -= learning_rate * bias_gradient[l]
+
+            avg_cost = total_cost / num_samples
+            print(f"Epoch {epoch+1}/{epochs} - Average Cost: {avg_cost:.4f}")
+
